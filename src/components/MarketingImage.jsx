fe48c3685bd85c374cc5f15fbb0b2801ad0cf983
Rory <99726710+spacecodboy@users.noreply.github.com>
2023-03-14 22:48:15 +0700

    edit market

--- src/components/MarketingImage.jsx
+++ src/components/MarketingImage.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 const MarketingImage = () => {
     return (
         <a onClick={() => window.open("https://github.com/spacecodboy")}>
-        <img style={{position:'fixed', width: '250px', height: '250px', display: 'flex', right:'30px', cursor:'pointer', bottom:'5%'}}  src={require('E:/НГТУ (НЭТИ)/2 КУРС/4 СЕМЕСТР/ТИМП/Лаба1/timp-lr-1/src/image/kvac.jpg')}/>
+        <img 
+        style={{position:'fixed', 
+        width: '9vw', 
+        height: '15vh', 
+        display: 'flex', 
+        right:'30px', 
+        cursor:'pointer', 
+        bottom:'5%'}}  
+        src={require('E:/НГТУ (НЭТИ)/2 КУРС/4 СЕМЕСТР/ТИМП/Лаба1/timp-lr-1/src/image/GitHub-Mark.png')}/>
       </a>
     );
 };
